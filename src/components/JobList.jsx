453633b462bb3f7b3882ed54a243d9c9685147b0
Createawish <bezlyudovalera1996@gmail.com>
2022-09-08 15:57:21 +0300

    Refactor: refactor filter reduser

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,13 +1,23 @@
-import {useSelector} from "react-redux";
+import {useSelector,useDispatch} from "react-redux";
 import { JobPosition } from './JobPosition';
 import {selectAllPositions} from "../store/position/position-selector";
+import {addFilter} from "../store/filres/filtres-actions";
 
 const JobList = () => {
-  const positions = useSelector(selectAllPositions)
+  const positions = useSelector(selectAllPositions);
+  const dispatch = useDispatch();
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  }
+
   return (
     <div className='job-list'>
       {positions.map(item => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition
+            handleAddFilter = {handleAddFilter}
+            key={item.id}
+            {...item} />
       ))}
     </div>
   )
