7577439e5c9c806ef51544b91da2ef28e77c4b7f
Createawish <bezlyudovalera1996@gmail.com>
2022-09-08 15:25:33 +0300

    Feature: add Filter reduser

--- src/store/root-reduser.js
+++ src/store/root-reduser.js
@@ -1,8 +1,9 @@
 import {combineReducers} from "redux";
 import {positionReducer} from "./position/position-reduser";
-
+import {filterReduser} from "./filres/filters-reduser";
 
 
 export const rootReducer = combineReducers({
     positions: positionReducer,
+    filters: filterReduser,
 })
